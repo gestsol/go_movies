a09b877aca21a2c067c008343952c325c0342360
Moreno Maria <marimorexx@gmail.com>
2021-08-11 20:20:47 +0200

    Added mongo models

--- lib/go_movie_web/controllers/resource_controller.ex
+++ lib/go_movie_web/controllers/resource_controller.ex
@@ -7,24 +7,38 @@ defmodule GoMovieWeb.ResourceController do
   action_fallback GoMovieWeb.FallbackController
 
   def index(conn, _params) do
-    resources = Content.list_resources()
-    render(conn, "index.json", resources: resources)
+    #resources = Content.list_resources()
+
+    resources_movies = Mongo.find(:mongo, "resources_movies", %{})
+
+    resources_movies
+    |> Enum.to_list()
+    |> IO.inspect
+
+    json(conn, %{resources_movies: "test"})
+
+   # render(conn, "index.json", resources: resources)
   end
 
-  def create(conn, %{"resource" => resource_params}) do
-    with {:ok, %Resource{} = resource} <- Content.create_resource(resource_params) do
-       # GENERATE SEQUENCE ID
-     {:ok , counter} = Mongo.find_one_and_update(:mongo, "counters", %{_id: "resource_id" }, %{"$inc": %{sequence_value: 1}})
+  def create(conn, %{"resource_movie" => resource_params}) do
 
-     IO.inspect(counter)
+    IO.inspect(resource_params)
 
-     Mongo.insert_one(:mongo, "resource", %{ resource_params: resource_params, id: counter["sequence_value"]})
+    # GENERATE SEQUENCE ID
+    {:ok , counter} = Mongo.find_one_and_update(:mongo, "counters", %{_id: "resource_id" }, %{"$inc": %{sequence_value: 1}})
+    resource_merge = Map.merge(resource_params, %{id: counter["sequence_value"] })
 
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", Routes.resource_path(conn, :show, resource))
-      |> render("show.json", resource: resource)
-    end
+    IO.inspect(counter)
+    IO.inspect(resource_merge)
+
+
+    Mongo.insert_one(:mongo, "resources_movies", %{ resource_params: resource_params, id: counter["sequence_value"]})
+
+      # conn
+      # |> put_status(:created)
+      # |> put_resp_header("location", Routes.resource_path(conn, :show, resource))
+      # |> render("show.json", resource: resource)
+      send_resp(conn, :no_content, "")
   end
 
   def show(conn, %{"id" => id}) do
