840ae7e0585d46003efc49927f1333f011160417
Moreno Maria <marimorexx@gmail.com>
2021-08-13 20:48:40 +0200

    Update(resourece_movies) = code refractoring

--- lib/go_movie_web/controllers/resource_controller.ex
+++ lib/go_movie_web/controllers/resource_controller.ex
@@ -1,8 +1,6 @@
 defmodule GoMovieWeb.ResourceController do
   use GoMovieWeb, :controller
 
-  alias GoMovie.Content
-  alias GoMovie.Content.Resource
 
   action_fallback GoMovieWeb.FallbackController
 
@@ -10,53 +8,44 @@ defmodule GoMovieWeb.ResourceController do
     resources_movies = Mongo.find(:mongo, "resources_movies", %{})
 
     resources_movies_filtered = resources_movies
-    |> Enum.to_list()
-    |> Enum.map(fn x ->
-      Map.delete(x, "_id")
-    end )
-    |> IO.inspect()
+    |> Enum.map(fn x -> Map.delete(x, "_id") end)
 
     json(conn, resources_movies_filtered)
-
   end
 
   def create(conn, %{"resource_movie" => resource_params}) do
 
     # GENERATE SEQUENCE ID
     {:ok , counter} = Mongo.find_one_and_update(:mongo, "counters", %{_id: "resource_id" }, %{"$inc": %{sequence_value: 1}})
-    resource_merge = Map.merge(resource_params, %{id: counter["sequence_value"] })
-    IO.inspect(counter)
+    resource_merge = Map.merge(resource_params, %{"id_resource" => counter["sequence_value"] })
 
-    #{:ok, resource_movie }  = Mongo.insert_one(:mongo, "resources_movies", %{ resource_params: resource_params, id: counter["sequence_value"]})
     {:ok, resource_movie }  = Mongo.insert_one(:mongo, "resources_movies", resource_merge)
+    #IO.inspect(resource_movie)
 
-    IO.inspect(resource_movie)
+    resources_movie_get = Mongo.find_one(:mongo, "resources_movies", %{id_resource: counter["sequence_value"]})
 
-    resource_movie
-    |> Enum.map()
-    |> IO.inspect()
-
-    send_resp(conn, :no_content, "")
-   # json(conn, resources_movie |> Map.delete("_id"))
-   #json(conn, resource_movie |> Map.delete("_id"))
+    json(conn, resources_movie_get |> Map.delete("_id"))
   end
 
   def show(conn, %{"id" => id}) do
-    resources_movie = Mongo.find_one(:mongo, "resources_movies", %{id: String.to_integer(id)})
+    resources_movie = Mongo.find_one(:mongo, "resources_movies", %{id_resource: String.to_integer(id)})
     json(conn, resources_movie |> Map.delete("_id"))
   end
 
-  def update(conn, %{"id" => id, "resource" => resource_params}) do
-    resource = Content.get_resource!(id)
+  def update(conn, %{"id" => id, "resource_movie" => resource_params}) do
+    {:ok, resource_movie} = Mongo.find_one_and_update(:mongo, "resources_movies", %{id_resource: String.to_integer(id)}, %{"$set": resource_params})
+   # IO.inspect(resource_movie)
 
+    resources_movie_get = Mongo.find_one(:mongo, "resources_movies", %{id_resource: String.to_integer(id)})
+
+    json(conn, resources_movie_get |> Map.delete("_id"))
   end
 
   def delete(conn, %{"id" => id}) do
-    {:ok, result} = Mongo.delete_one(:mongo, "resources_movies", %{id: String.to_integer(id)})
-    IO.inspect( result)
-    send_resp(conn, :no_content, "")
+    {:ok, result} = Mongo.delete_one(:mongo, "resources_movies", %{id_resource: String.to_integer(id)})
+    #IO.inspect( result)
 
-    #json(conn, %{"result": result["acknowledged"]})
+    send_resp(conn, :no_content, "")
   end
 
 end
