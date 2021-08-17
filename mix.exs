ff6c56bbc52b7faeea42a1b5f065566889be54f4
Moreno Maria <marimorexx@gmail.com>
2021-08-17 19:56:45 +0200

    Update: disable Mongo server configuration.

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule GoMovie.MixProject do
   def application do
     [
       mod: {GoMovie.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :mongodb]
     ]
   end
 
