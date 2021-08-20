f10976771a9475fff2a09762d8f8b93c1f3e2b7a
Moreno Maria <marimorexx@gmail.com>
2021-08-20 21:21:50 +0200

    Fix(m_resource_movies): update sintaxis

---
+++ lib/go_movie_web/controllers/m_resource_movie_controller.ex
@@ -0,0 +1,36 @@
+defmodule GoMovieWeb.MResourceMovieController do
+  use GoMovieWeb, :controller
+
+  alias GoMovie.MongoUtils
+
+  action_fallback GoMovieWeb.FallbackController
+
+  @collection_name "resources_movies"
+
+  def index(conn, _params) do
+    resources_movies = MongoUtils.find_all(@collection_name)
+    json(conn, resources_movies)
+  end
+
+  def create(conn, %{"resource_movie" => resource_params}) do
+    resource_movie = resource_params |> MongoUtils.insert_one(@collection_name)
+    json(conn, resource_movie)
+  end
+
+  def show(conn, %{"id" => id}) do
+    resources_movie = MongoUtils.get_by_id(id, @collection_name)
+    json(conn, resources_movie)
+  end
+
+  def update(conn, %{"id" => id, "resource_movie" => resource_params}) do
+    resource_movie = resource_params |> MongoUtils.update(@collection_name, id)
+    json(conn, resource_movie)
+  end
+
+  def delete(conn, %{"id" => id}) do
+    with {:ok, _} <- MongoUtils.delete(id, @collection_name) do
+      send_resp(conn, :no_content, "")
+    end
+  end
+
+end
