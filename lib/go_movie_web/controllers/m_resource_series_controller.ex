d741c14e779d2ce8920d76135c91b04412b14271
Leonardo Ramos <leorrc96@gmail.com>
2021-08-26 12:36:34 -0400

    update(m_resources_series): add validation on series update in genders field

--- lib/go_movie_web/controllers/m_resource_series_controller.ex
+++ lib/go_movie_web/controllers/m_resource_series_controller.ex
@@ -17,7 +17,6 @@ defmodule GoMovieWeb.MResourceSerieController do
   end
 
   def create(conn, %{"resource_serie" => resource_params}) do
-
     genders = resource_params["genders"]
 
     case MongoUtils.validate_genders(genders) do
@@ -47,89 +46,120 @@ defmodule GoMovieWeb.MResourceSerieController do
   end
 
   def add_season(conn, %{"season" => season, "serie_id" => serie_id}) do
-
     case Serie.add_season(season, serie_id) do
       {:ok, season} ->
         json(conn, season)
 
       {:error, msg} ->
         conn
-          |> put_status(400)
-          |> json(%{error: msg})
+        |> put_status(400)
+        |> json(%{error: msg})
     end
   end
 
   def update_season(conn, %{"season" => season, "serie_id" => serie_id, "season_id" => season_id}) do
-
     case Serie.update_season(season, serie_id, season_id) do
       {:ok, season} ->
         json(conn, season)
 
       {:error, msg} ->
         conn
-          |> put_status(400)
-          |> json(%{error: msg})
+        |> put_status(400)
+        |> json(%{error: msg})
     end
   end
 
   def add_chapter(conn, %{"chapter" => chapter, "serie_id" => serie_id, "season_id" => season_id}) do
-
     case Serie.add_chapter(chapter, serie_id, season_id) do
       {:ok, chapter} ->
         json(conn, chapter)
 
       {:error, msg} ->
         conn
-          |> put_status(400)
-          |> json(%{error: msg})
+        |> put_status(400)
+        |> json(%{error: msg})
     end
   end
 
-  def update_chapter(conn, %{"chapter" => chapter, "serie_id" => serie_id, "season_id" => season_id, "chapter_id" => chapter_id}) do
-
+  def update_chapter(conn, %{
+        "chapter" => chapter,
+        "serie_id" => serie_id,
+        "season_id" => season_id,
+        "chapter_id" => chapter_id
+      }) do
     case Serie.update_series_chapter(chapter, serie_id, season_id, chapter_id) do
       {:ok, updated_chapter} ->
         json(conn, updated_chapter)
 
       {:error, msg} ->
         conn
-          |> put_status(400)
-          |> json(%{error: msg})
+        |> put_status(400)
+        |> json(%{error: msg})
     end
   end
 
-  def delete_chapter(conn, %{"serie_id" => serie_id, "season_id" => season_id, "chapter_id" => chapter_id}) do
-
+  def delete_chapter(conn, %{
+        "serie_id" => serie_id,
+        "season_id" => season_id,
+        "chapter_id" => chapter_id
+      }) do
     case Serie.delete_chapter(serie_id, season_id, chapter_id) do
       {:ok, _msg} ->
         send_resp(conn, 204, "")
 
       {:error, msg} ->
         conn
-          |> put_status(400)
-          |> json(%{error: msg})
+        |> put_status(400)
+        |> json(%{error: msg})
     end
   end
 
   def update(conn, %{"id" => id, "resource_serie" => resource_params}) do
-
     if resource_params["seasons"] do
-      conn
-      |> put_status(400)
-      |> json(%{msg: "Is not allowed to update series seasons."})
+      handle_season_param_on_update(conn)
     else
-      resource_serie =
-        resource_params
-        |> MongoUtils.update(@collection_name, id)
-        |> Serie.parse_seasons_and_chapters_ids()
-
-        json(conn, resource_serie)
+      handle_genders_params_on_update(conn, id, resource_params)
     end
   end
 
   def delete(conn, %{"id" => id}) do
     with {:ok, _} <- MongoUtils.delete(id, @collection_name) do
       send_resp(conn, :no_content, "")
+    end
+  end
+
+  def handle_season_param_on_update(conn) do
+    conn
+      |> put_status(400)
+      |> json(%{msg: "Is not allowed to update series seasons."})
+  end
+
+  def handle_genders_params_on_update(conn, id, resource_params) do
+    genders = resource_params["genders"]
+
+    if genders do
+      case MongoUtils.validate_genders(genders) do
+        {:error, msg} ->
+          conn
+          |> put_status(400)
+          |> json(%{error: msg})
+
+        {:ok, db_genders} ->
+          resource_serie =
+            resource_params
+            |> Map.put("genders", db_genders)
+            |> MongoUtils.update(@collection_name, id)
+            |> Serie.parse_seasons_and_chapters_ids()
+
+          json(conn, resource_serie)
+      end
+    else
+      resource_serie =
+        resource_params
+        |> MongoUtils.update(@collection_name, id)
+        |> Serie.parse_seasons_and_chapters_ids()
+
+      json(conn, resource_serie)
     end
   end
 end
